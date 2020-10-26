34d2f9128e38dc4741d6e56d327a3953d69e5557
Beniamin Lobodziec <thetroleklolekpl@gmail.com>
2020-10-26 10:51:04 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
-// Lets gooooo
 
-int main(void) {
-// hello
-	return 0;
+int main(void){
+  char verbs [10][10] = {"er", "bliver", "går", "får", "synes","har","skal","vil","siger","kom" };
+
+  char test_sentence [50] = "Jeg er glad, fordi jeg skal hjem"; 
+
+  strstr(test_sentence, verbs));
+
+  int yes = strcmp(test_sentence,verbs);
+
+
+
 }
