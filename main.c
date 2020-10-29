189e6144ce2f8840e33cf60189ebe0af726b48aa
Frederik <frfr19ae@student.cbs.dk>
2020-10-29 09:54:03 +0100

    Parantes error

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(void){
 
   char test_sentence [50] = "Jeg er glad, fordi jeg skal hjem"; 
 
-  strstr(test_sentence, verbs));
+  strstr(test_sentence, verbs);
 
   int yes = strcmp(test_sentence,verbs);
 
