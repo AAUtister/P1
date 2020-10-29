8482f99ff23538335f14d37083a25255fc4c0a71
Beniamin Lobodziec <thetroleklolekpl@gmail.com>
2020-10-29 14:41:39 +0100

    Update main.c
    Programmet finder mellemrummet mellem orderne og fjerner den. Bagefter programmet undersøger om der er et ord som er ligmed vores verb som er: "er". Når der findes en "er" bliver den printet ud.

--- main.c
+++ main.c
@@ -1,16 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 
+int main(void)
+{
+  // char nothing[2] = " ";
+  char nothing[1] = " ";
+  char word[4];
+  char verbs[4] = "er";
+  char str1[50];
+  char test_sentence[50] = "Jeg er glad, fordi jeg dum";
+  char *token;
+  int ret;
+  // space = strtok(test_sentence, nothing);
 
-int main(void){
-  char verbs [10][10] = {"er", "bliver", "går", "får", "synes","har","skal","vil","siger","kom" };
+  token = strtok(test_sentence, nothing);
+  // int yes = strcmp(test_sentence, verbs);
+  strcpy(str1, verbs);
+  strcpy(word, token);
 
-  char test_sentence [50] = "Jeg er glad, fordi jeg skal hjem"; 
+  while (token != NULL)
+  {
+    ret = strcmp(verbs, token);
 
-  strstr(test_sentence, verbs);
+    if (ret == 0)
+    {
+      printf("komma \n");
+      printf("Token = %s og str1 = %s \n", token, str1);
+      token = strtok(NULL, nothing);
+    }
+    else
+    {
 
-  int yes = strcmp(test_sentence,verbs);
+      token = strtok(NULL, nothing);
+    }
+  }
 
-
-int YEP_COCK;
+  // int YEP_COCK;
 }
