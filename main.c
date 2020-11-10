c8ca6292aa0563ad4e09ff52650bb50219304dfe
Frederik <frfr19ae@student.cbs.dk>
2020-11-10 13:45:19 +0100

    Updated main.c

--- main.c
+++ main.c
@@ -44,33 +44,8 @@ int main() {
         printf("\n%s", tekst_array[x]);
     }
 
-    printf("\n%s", tekst_array[0]);
-
-    // printf("\n%s",tekst_array[31]); //Eksempel
+    printf("\n%s", tekst_array[31]); // Eksempel på at tage et ord
+    printf("\n%c", tekst_array[31][0]); // Eksempel på at tage et bogstav
     
     return 0;
-}
-
-
-int func2 () {
-   char str1[15];
-   char str2[15];
-   int ret;
-
-   strcpy(str1, "Å");
-   strcpy(str2, "Æ");
-
-   ret = strcmp(str1, str2);
-
-   printf("%d", ret);
-   
-   if(ret < 0) {
-      printf("str1 is less than str2");
-   } else if(ret > 0) {
-      printf("str2 is less than str1");
-   } else {
-      printf("str1 is equal to str2");
-   }
-   
-   return(0);
 }
