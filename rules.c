6417643c000d5a08f7a4387f225d581036f051d4
BlackChakers <thetroleklolekpl@gmail.com>
2020-12-09 12:03:53 +0100

    jeg slettede ukommentede tingene

--- rules.c
+++ rules.c
@@ -19,13 +19,10 @@ void rules(char *tekst_array[], int found_rule[])
         for (int j = 0; j < x; j++)
         {
             int ret = str_cmp(tekst_array[i], hv_array[j]);
-            // printf("tekst = %s -- hv_array = %s \n", tekst_array[i], hv_array[j]);
             if (ret == 0)
             {
-                // printf("\nYES ---i = %d and j = %d \n tekst = %s og hv = %s \n", i, j, tekst_array[i], hv_array[j]);
                 found_rule[i] = ret;
 
-                // printf("\n \n yes = %d \n \n", found_rule[i]);
                 break;
             }
             else
