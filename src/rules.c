6b63801eaa32958c4165c6d6d27db47694aaf9db
vonfrohn <73224292+vonfrohn@users.noreply.github.com>
2020-12-15 12:46:20 +0100

    pik

--- src/rules.c
+++ src/rules.c
@@ -7,7 +7,8 @@
 int str_cmp(const char *str1, const char *str2);
 void rules(char *tekst_array[], int found_rule[])
 {
-    char *hv_array[17] = {"hvem", "hvad", "hvornår", "hvorfor", "hvordan", "hvor", "hvilken", "hvorfra", "hvorhen", "hvilket", "hvilke", "fordi", "at", "end", "man", "for", "men"};
+    int x = 11;
+    char *hv_array[x] = {"hvem", "hvad", "hvornår", "hvorfor", "hvordan", "hvor", "hvilken", "hvorfra", "hvorhen", "hvilket", "hvilke"};
     int c = 0;
 
     while (tekst_array[c] != NULL)
@@ -15,23 +16,14 @@ void rules(char *tekst_array[], int found_rule[])
         c++;
     }
     printf("Antal ord i tekst_array: %d \n", c);
-
-    // int banan = sizeof(hv_array) / sizeof(int);
-    // printf("%i\n", banan);
-    int x = 17;
-
     for (int i = 0; i < c; i++)
     {
         for (int j = 0; j < x; j++)
         {
             int ret = str_cmp(tekst_array[i], hv_array[j]);
-            // printf("tekst = %s -- hv_array = %s \n", tekst_array[i], hv_array[j]);
             if (ret == 0)
             {
-                // printf("\nYES ---i = %d and j = %d \n tekst = %s og hv = %s \n", i, j, tekst_array[i], hv_array[j]);
                 found_rule[i] = ret;
-
-                // printf("\n \n yes = %d \n \n", found_rule[i]);
                 break;
             }
             else
